effd09eca0d71b5a461932df3e0d81b1f3014527
Darwing <Darwingx990@gmail.com>
2024-04-05 12:53:08 -0500

    Agregué el modelo listar con la funcion Listar y Agregar

--- Gestion_hospital/src/main/java/io/riwi/model/ModelCita.java
+++ Gestion_hospital/src/main/java/io/riwi/model/ModelCita.java
@@ -6,48 +6,45 @@ import io.riwi.entity.Medico;
 import io.riwi.entity.Paciente;
 
 import javax.swing.*;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ModelCita {
-    public static List<Cita> listarPaciente() {
-        List<Cita >citas = new ArrayList<>();
-        PreparedStatement psPaciente;
-        ResultSet rsPaciente;
+    public static List<Cita> listarCita() {
+        List<Cita> citas = new ArrayList<>();
+        PreparedStatement psCita;
+        ResultSet rsCita;
         Connection connection = ConfigDB.openConnection();
         String sql = "SELECT * FROM cita INNER JOIN paciente ON paciente.id_paciente = cita.fk_id_paciente " +
                 "INNER JOIN medico ON medico.id_medico = cita.fk_id_medico ORDER BY id_paciente;";
         try {
-            psPaciente = connection.prepareStatement(sql);
-            rsPaciente = psPaciente.executeQuery();
+            psCita = connection.prepareStatement(sql);
+            rsCita = psCita.executeQuery();
 
-            while (rsPaciente.next()) {
+            while (rsCita.next()) {
                 Cita cita = new Cita();
-                Medico medico  = new Medico();
+                Medico medico = new Medico();
                 Paciente paciente = new Paciente();
 
-                cita.setIdCitas(rsPaciente.getInt("cita.id_cita"));
-                cita.setFechaCita(rsPaciente.getString("cita.fecha_cita"));
-                cita.setHoraCita(rsPaciente.getString("cita.hora_cita"));
-                cita.setIdPaciente(rsPaciente.getInt("cita.fk_id_paciente"));
-                cita.setIdMedico(rsPaciente.getInt("cita.fk_id_medico"));
-                cita.setMotivoCita(rsPaciente.getString("cita.motivo_cita"));
+                cita.setIdCitas(rsCita.getInt("cita.id_cita"));
+                cita.setFechaCita(rsCita.getString("cita.fecha_cita"));
+                cita.setHoraCita(rsCita.getString("cita.hora_cita"));
+                cita.setIdPaciente(rsCita.getInt("cita.fk_id_paciente"));
+                cita.setIdMedico(rsCita.getInt("cita.fk_id_medico"));
+                cita.setMotivoCita(rsCita.getString("cita.motivo_cita"));
 
 
-                paciente.setIdPaciente(rsPaciente.getInt("paciente.id_paciente"));
-                paciente.setNombrePaciente(rsPaciente.getString("paciente.nombre"));
-                paciente.setApellidosPaciente(rsPaciente.getString("paciente.apellidos"));
-                paciente.setFechaNaciemiento(rsPaciente.getString("paciente.fecha_nacimiento"));
-                paciente.setDocumentoIdentidad(rsPaciente.getString("paciente.documento_identidad"));
+                paciente.setIdPaciente(rsCita.getInt("paciente.id_paciente"));
+                paciente.setNombrePaciente(rsCita.getString("paciente.nombre"));
+                paciente.setApellidosPaciente(rsCita.getString("paciente.apellidos"));
+                paciente.setFechaNaciemiento(rsCita.getString("paciente.fecha_nacimiento"));
+                paciente.setDocumentoIdentidad(rsCita.getString("paciente.documento_identidad"));
 
-                medico.setIdMedico(rsPaciente.getInt("medico.id_medico"));
-                medico.setNombre(rsPaciente.getString("medico.nombre"));
-                medico.setApellidos(rsPaciente.getString("medico.apellidos"));
-                medico.setIdEspecialidad(rsPaciente.getInt("medico.fk_id_especialidad"));
+                medico.setIdMedico(rsCita.getInt("medico.id_medico"));
+                medico.setNombre(rsCita.getString("medico.nombre"));
+                medico.setApellidos(rsCita.getString("medico.apellidos"));
+                medico.setIdEspecialidad(rsCita.getInt("medico.fk_id_especialidad"));
 
                 cita.setObjPaciente(paciente);
                 cita.setObjMedico(medico);
@@ -59,21 +56,26 @@ public class ModelCita {
         } finally {
             ConfigDB.closeConnection();
         }
-            return citas;
+        return citas;
     }
 
-    public static boolean agregarPaciente(Paciente paciente) {
-        PreparedStatement psPaciente;
-        Connection connection = ConfigDB.openConnection();
-        String sql = "INSERT INTO paciente(nombre, apellidos, fecha_nacimiento,documento_identidad) VALUES (?, ?, ?, ?);";
-        try {
-            psPaciente = connection.prepareStatement(sql);
+    public static boolean agregarCita(Cita cita) {
+        PreparedStatement psCita;
 
-            psPaciente.setString(1, paciente.getNombrePaciente());
-            psPaciente.setString(2, paciente.getApellidosPaciente());
-            psPaciente.setString(3, paciente.getFechaNaciemiento());
-            psPaciente.setString(4, paciente.getDocumentoIdentidad());
-            psPaciente.execute();
+        Connection connection = ConfigDB.openConnection();
+        String sql = "INSERT INTO cita(fk_id_paciente, fk_id_Medico, fecha_cita, hora_cita, motivo_cita) VALUES (?, ?, ?, ?, ?) INNER JOIN paciente ON paciente.id_paciente = cita.fk_id_paciente ";
+        try {
+            psCita = connection.prepareStatement(sql);
+            Medico medico = new Medico();
+            Paciente paciente = new Paciente();
+            psCita.setInt(1, cita.getIdPaciente());
+
+            psCita.setInt(2, cita.getIdMedico());
+            psCita.setDate(3, Date.valueOf(cita.getFechaCita()));
+            psCita.setTime(4, Time.valueOf(cita.getHoraCita()));
+            psCita.setString(5, cita.getMotivoCita());
+            psCita.execute();
+
             return true;
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Error al agregar paciente >> " + e.getMessage());
@@ -124,7 +126,7 @@ public class ModelCita {
     }
 
     public static void main(String[] args) {
-        listarPaciente();
+        listarCita();
 
 //        Paciente paciente1 = new Paciente("Gabriel", "Serna", "1996-06-01", "1077481554");
 //        agregarPaciente(paciente1);
@@ -134,9 +136,9 @@ public class ModelCita {
 //        modificarPaciente(pcteModificar);
 //        listarPaciente();
 
-        Paciente pcteEliminar = new Paciente(5);
-        eliminarPaciente(pcteEliminar);
-        listarPaciente();
+//        Paciente pcteEliminar = new Paciente(5);
+//        eliminarPaciente(pcteEliminar);
+//        listarPaciente();
 
     }
 
